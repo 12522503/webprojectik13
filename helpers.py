f72ca6939d5e7119f77c793b2541d20e43d4efd4
Jesse van der Sluis <jvds22@gmail.com>
2020-01-15 10:27:26 +0000

    apology

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 def apology (message, code=400):
-    return render_template("apology.html", top=code, bottom=message)
+    return render_template("apology.html", top=code, bottom=message, code)
 
 
 
